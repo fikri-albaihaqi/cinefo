f34f67ba4162ae6d9ced7dfcfe05f22b639305d3
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2022-10-14 22:09:23 +0700

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const url = 'https://api.themoviedb.org/3';
-const apiKey = '56daad2a468cbf938113f1e79c8ff61c';
+const apiKey = '';
 
 export const getTrending = () => axios.get(`${url}/trending/all/day?api_key=${apiKey}`);
