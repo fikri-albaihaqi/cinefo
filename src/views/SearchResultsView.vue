cb6f3149b06e880286b7fe0cfafec47c54901725
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-03-22 21:44:26 +0700

    feat: add a message when the search has no result

--- src/views/SearchResultsView.vue
+++ src/views/SearchResultsView.vue
@@ -22,6 +22,7 @@ export default {
     '$route.params.query': {
       async handler(newValue) {
         this.search(newValue);
+        console.log(this.searchResults[0])
       },
       immediate: true
     }
@@ -32,9 +33,12 @@ export default {
 <template>
   <div class="w-[90vw] md:w-[80vw] m-auto lg:pt-32">
     <h1 class="font-bold text-2xl" data-aos="fade-up" data-aos-delay="300">Search Result(s)</h1>
-    <div class="grid gap-4 grid-cols-3 md:grid-cols-5 mt-4 lg:mt-8">
+    <div v-if="searchResults.length > 0" class="grid gap-4 grid-cols-3 md:grid-cols-5 mt-4 lg:mt-8">
       <CardItem :api-data="searchResults" v-for="(item, index) in searchResults" :key="`item-${index}`" :index="index"
         :margin="'mx-4 my-8'" :hide="'hidden'" :media-type="searchResults[index].media_type" data-aos="fade-up" data-aos-delay="300" />
+    </div>
+    <div v-else class="h-screen flex justify-center mt-32 text-4xl">
+      <h1>Sorry we can't find what you're looking for.</h1>
     </div>
   </div>
 </template>
